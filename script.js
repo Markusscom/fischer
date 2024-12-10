f30da4d9583e2eb6387d716e2cd4767e210d38ce
Your Name <you@example.com>
2024-12-10 16:06:12 +0200

    fischer v1.0.3

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const CastButton = document.querySelector(cast)
+const CastButton = document.getElementById("cast")
 
-CastButton.addEventListener("click", castHandle)
-
-function castHandle() {
-}
+CastButton.addEventListener("click", () => {
+    CastButton.classlist.add('SigmaAnimation')
+    CastButton.addEventListener('animationend', () => {
+        CastButton.classlist.remove('SigmaAnimation');
+    });
+});
